625a7b0a04fb2c2ce1ceb07f03997fbbf44d3e40
simonssonmattias-ops <simonsson.mattias@gmail.com>
2026-05-05 06:11:11 +0200

    Update supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,1 +1,7 @@
-
+import { createClient } from '@supabase/supabase-js'
+ 
+const supabaseUrl = 'https://xybfafzonmadtikzryor.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh5YmZhZnpvbm1hZHRpa3pyeXNyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY5MjkyODMsImV4cCI6MjA5MjUwNTI4M30.mO1E8LTraXnTLkcCKPMOXHK0kviQZjasAipEOV5Dq8Y'
+ 
+export const supabase = createClient(supabaseUrl, supabaseKey)
+ 
